3b38493815a0a5f773b772d5bb1c263cbcd52498
Dredayduncan <dredayduncan@gmail.com>
2020-03-06 12:00:06 +0000

    Completed the country class

--- Country.java
+++ Country.java
@@ -1,35 +1,35 @@
-import java.util.ArrayList;
 /**
  * 
  * @author Bryan Tetteh Ayertey
  *
  */
+package Career_Fair_Challenge;
+import java.util.ArrayList;
+
 public class Country {
 	String name;
-	ArrayList<Integer> infections;
+	ArrayList<String[]> infections;
 	int population;
 	int count;
-	/**
-	 * Default constructor
-	 */
-	private Country() {
-		
-	}
+
 	/**
 	 * The constructor for the Country calls that takes the countries name and poplutaion.
 	 * @param name The name of the country
 	 * @param population The population of the country
 	 */
-	private Country(String name, int population) {
+	public Country(String name, int population) {
 		this.name = name;
 		this.population = population;
+		count = 0;
+		infections = new ArrayList<>();
 	
 	}
+
 	/**
 	 * Get tbe number of infections that occured in the country
 	 * @return the number of infections
 	 */
-	public int getCount() {
+	public int getInfectionCount() {
 		return count;
 	}
 	
@@ -48,13 +48,15 @@ public class Country {
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	/**
-	 * Get the population the country
+	 * Get the population of the country
 	 * @return the population of the country
 	 */
 	public int getPopulation() {
 		return population;
 	}
+
 	/**
 	 * Get the population the country
 	 * @param population The population of the country
@@ -62,13 +64,18 @@ public class Country {
 	public void setPopulation(int population) {
 		this.population = population;
 	}
+
 	/**
 	 * Add a day's infection cases count to the countries list of cases and count
-	 * @param infection The day's infection count
+	 * @param date The date
+	 * @param numConfCases The number of confirmed cases
+	 * @param deaths The number of deaths
 	 */
-	public void addInfection(Integer infection) {
+	public void addInfection(String date, String numConfCases, String deaths) {
+		count += Integer.parseInt(numConfCases);
+		String[] infection = {date, numConfCases, deaths};
 		infections.add(infection);
-		count = count + infection;
+
 	}
 	
 }
--- Utilities.java
+++ Utilities.java
@@ -1,10 +1,31 @@
+/**
+ * @author Andrew Duncan
+ */
 package Career_Fair_Challenge;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Hashtable;
 import java.util.Scanner;
 
 public class Utilities {
-    Scanner input;
+    //Store the countries by their name and Country object in a hashtable
+    private Hashtable<String, Country> countries = new Hashtable<>();
 
-    
+    //Declare the variables for the input files
+    Scanner populationData, infectionCases;
 
+
+    /**
+     *
+     * @param countryData Path to the csv file containing the details about countries, population, country code and year
+     * @param infectionData Path to the csv file containing the details about infections each day in each country.
+     * @throws FileNotFoundException
+     */
+    public Utilities(String countryData, String infectionData) throws FileNotFoundException {
+        populationData = new Scanner(new File(countryData));
+        infectionCases = new Scanner(new File(infectionData));
+
+
+    }
 }
